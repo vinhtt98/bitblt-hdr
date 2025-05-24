6b7e77715a4e35048003eb06d3cc096f2a4f1b53
GEEKiDoS <geek_ds@foxmail.com>
2025-05-24 13:51:26 +0800

    use macro on dllproxy

--- dllproxy/version.asm
+++ dllproxy/version.asm
@@ -1,183 +1,41 @@
 ifndef rax
 .686
 .model flat, stdcall
-.code
-
-extern GetFileVersionInfoA_Original:DWORD
-extern GetFileVersionInfoByHandle_Original:DWORD
-extern GetFileVersionInfoExA_Original:DWORD
-extern GetFileVersionInfoExW_Original:DWORD
-extern GetFileVersionInfoSizeA_Original:DWORD
-extern GetFileVersionInfoSizeExA_Original:DWORD
-extern GetFileVersionInfoSizeExW_Original:DWORD
-extern GetFileVersionInfoSizeW_Original:DWORD
-extern GetFileVersionInfoW_Original:DWORD
-extern VerFindFileA_Original:DWORD
-extern VerFindFileW_Original:DWORD
-extern VerInstallFileA_Original:DWORD
-extern VerInstallFileW_Original:DWORD
-extern VerLanguageNameA_Original:DWORD
-extern VerLanguageNameW_Original:DWORD
-extern VerQueryValueA_Original:DWORD
-extern VerQueryValueW_Original:DWORD
-
-GetFileVersionInfoA_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoA_Original
-GetFileVersionInfoA_EXPORT endp
-
-GetFileVersionInfoByHandle_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoByHandle_Original
-GetFileVersionInfoByHandle_EXPORT endp
-
-GetFileVersionInfoExA_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoExA_Original
-GetFileVersionInfoExA_EXPORT endp
-
-GetFileVersionInfoExW_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoExW_Original
-GetFileVersionInfoExW_EXPORT endp
-
-GetFileVersionInfoSizeA_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoSizeA_Original
-GetFileVersionInfoSizeA_EXPORT endp
-
-GetFileVersionInfoSizeExA_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoSizeExA_Original
-GetFileVersionInfoSizeExA_EXPORT endp
-
-GetFileVersionInfoSizeExW_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoSizeExW_Original
-GetFileVersionInfoSizeExW_EXPORT endp
-
-GetFileVersionInfoSizeW_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoSizeW_Original
-GetFileVersionInfoSizeW_EXPORT endp
-
-GetFileVersionInfoW_EXPORT proc
-	jmp DWORD ptr GetFileVersionInfoW_Original
-GetFileVersionInfoW_EXPORT endp
-
-VerFindFileA_EXPORT proc
-	jmp DWORD ptr VerFindFileA_Original
-VerFindFileA_EXPORT endp
-
-VerFindFileW_EXPORT proc
-	jmp DWORD ptr VerFindFileW_Original
-VerFindFileW_EXPORT endp
-
-VerInstallFileA_EXPORT proc
-	jmp DWORD ptr VerInstallFileA_Original
-VerInstallFileA_EXPORT endp
-
-VerInstallFileW_EXPORT proc
-	jmp DWORD ptr VerInstallFileW_Original
-VerInstallFileW_EXPORT endp
-
-VerLanguageNameA_EXPORT proc
-	jmp DWORD ptr VerLanguageNameA_Original
-VerLanguageNameA_EXPORT endp
-
-VerLanguageNameW_EXPORT proc
-	jmp DWORD ptr VerLanguageNameW_Original
-VerLanguageNameW_EXPORT endp
-
-VerQueryValueA_EXPORT proc
-	jmp DWORD ptr VerQueryValueA_Original
-VerQueryValueA_EXPORT endp
-
-VerQueryValueW_EXPORT proc
-	jmp DWORD ptr VerQueryValueW_Original
-VerQueryValueW_EXPORT endp
-
-else
-.code
-extern GetFileVersionInfoA_Original:QWORD
-extern GetFileVersionInfoByHandle_Original:QWORD
-extern GetFileVersionInfoExA_Original:QWORD
-extern GetFileVersionInfoExW_Original:QWORD
-extern GetFileVersionInfoSizeA_Original:QWORD
-extern GetFileVersionInfoSizeExA_Original:QWORD
-extern GetFileVersionInfoSizeExW_Original:QWORD
-extern GetFileVersionInfoSizeW_Original:QWORD
-extern GetFileVersionInfoW_Original:QWORD
-extern VerFindFileA_Original:QWORD
-extern VerFindFileW_Original:QWORD
-extern VerInstallFileA_Original:QWORD
-extern VerInstallFileW_Original:QWORD
-extern VerLanguageNameA_Original:QWORD
-extern VerLanguageNameW_Original:QWORD
-extern VerQueryValueA_Original:QWORD
-extern VerQueryValueW_Original:QWORD
-
-GetFileVersionInfoA_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoA_Original
-GetFileVersionInfoA_EXPORT endp
-
-GetFileVersionInfoByHandle_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoByHandle_Original
-GetFileVersionInfoByHandle_EXPORT endp
-
-GetFileVersionInfoExA_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoExA_Original
-GetFileVersionInfoExA_EXPORT endp
-
-GetFileVersionInfoExW_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoExW_Original
-GetFileVersionInfoExW_EXPORT endp
-
-GetFileVersionInfoSizeA_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoSizeA_Original
-GetFileVersionInfoSizeA_EXPORT endp
-
-GetFileVersionInfoSizeExA_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoSizeExA_Original
-GetFileVersionInfoSizeExA_EXPORT endp
-
-GetFileVersionInfoSizeExW_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoSizeExW_Original
-GetFileVersionInfoSizeExW_EXPORT endp
-
-GetFileVersionInfoSizeW_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoSizeW_Original
-GetFileVersionInfoSizeW_EXPORT endp
-
-GetFileVersionInfoW_EXPORT proc
-	jmp QWORD ptr GetFileVersionInfoW_Original
-GetFileVersionInfoW_EXPORT endp
-
-VerFindFileA_EXPORT proc
-	jmp QWORD ptr VerFindFileA_Original
-VerFindFileA_EXPORT endp
-
-VerFindFileW_EXPORT proc
-	jmp QWORD ptr VerFindFileW_Original
-VerFindFileW_EXPORT endp
-
-VerInstallFileA_EXPORT proc
-	jmp QWORD ptr VerInstallFileA_Original
-VerInstallFileA_EXPORT endp
-
-VerInstallFileW_EXPORT proc
-	jmp QWORD ptr VerInstallFileW_Original
-VerInstallFileW_EXPORT endp
-
-VerLanguageNameA_EXPORT proc
-	jmp QWORD ptr VerLanguageNameA_Original
-VerLanguageNameA_EXPORT endp
-
-VerLanguageNameW_EXPORT proc
-	jmp QWORD ptr VerLanguageNameW_Original
-VerLanguageNameW_EXPORT endp
-
-VerQueryValueA_EXPORT proc
-	jmp QWORD ptr VerQueryValueA_Original
-VerQueryValueA_EXPORT endp
-
-VerQueryValueW_EXPORT proc
-	jmp QWORD ptr VerQueryValueW_Original
-VerQueryValueW_EXPORT endp
-
 endif
+
+.code
+
+do_proxy macro func
+	ifdef rax
+		extern &func&_Original:QWORD
+		&func&_EXPORT proc
+			jmp QWORD ptr &func&_Original
+		&func&_EXPORT endp
+	else
+		extern &func&_Original:DWORD
+		&func&_EXPORT proc
+			jmp DWORD ptr &func&_Original
+		&func&_EXPORT endp
+	endif
+endm
+
+do_proxy GetFileVersionInfoA
+do_proxy GetFileVersionInfoByHandle
+do_proxy GetFileVersionInfoExA
+do_proxy GetFileVersionInfoExW
+do_proxy GetFileVersionInfoSizeA
+do_proxy GetFileVersionInfoSizeExA
+do_proxy GetFileVersionInfoSizeExW
+do_proxy GetFileVersionInfoSizeW
+do_proxy GetFileVersionInfoW
+do_proxy VerFindFileA
+do_proxy VerFindFileW
+do_proxy VerInstallFileA
+do_proxy VerInstallFileW
+do_proxy VerLanguageNameA
+do_proxy VerLanguageNameW
+do_proxy VerQueryValueA
+do_proxy VerQueryValueW
 
 end
 
